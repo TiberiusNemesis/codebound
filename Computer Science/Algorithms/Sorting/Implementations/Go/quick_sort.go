7c29262ed6d98a8a7a805e5e7d42737fdb9fb456
Tiberius Dourado <tiberiusthefifth@gmail.com>
2024-01-06 00:41:25 -0300

    Implement tail call optimization in quick sort

--- Computer Science/Algorithms/Sorting/Implementations/Go/quick_sort.go
+++ Computer Science/Algorithms/Sorting/Implementations/Go/quick_sort.go
@@ -1,36 +1,36 @@
 package sorting
 
-func QuickSort(arr []int) []int {
-    if len(arr) < 2 {
-        return arr
-    }
-
-    pivot := medianOfThree(arr, 0, len(arr)/2, len(arr)-1)
-
-    left, right := 0, len(arr)-1
-    for left <= right {
-        for arr[left] < pivot {
-            left++
-        }
-        for arr[right] > pivot {
-            right--
-        }
-        if left <= right {
-            arr[left], arr[right] = arr[right], arr[left]
-            left++
-            right--
-        }
-    }
-
-    if 0 < right {
-        QuickSort(arr[:right+1])
-    }
-    if left < len(arr) {
-        QuickSort(arr[left:])
-    }
-
-    return arr
+func QuickSort(arr []int) {
+    quickSortHelper(arr, 0, len(arr)-1)
 }
+
+func quickSortHelper(arr []int, start, end int) {
+    for start < end {
+        pivotIndex := medianOfThree(arr, start, start+(end-start)/2, end)
+		pivotNewIndex := partition(arr, start, end, pivotIndex)
+
+       quickSortHelper(arr, start, pivotNewIndex - 1)
+	   start = pivotNewIndex + 1
+    }
+}
+
+func partition(arr []int, start, end, pivotIndex int) int {
+    pivot := arr[pivotIndex]
+
+    arr[pivotIndex], arr[end] = arr[end], arr[pivotIndex]
+	storeIndex := start
+
+    for i := start; i < end; i++ {
+		if arr[i] < pivot {
+			arr[i], arr[storeIndex] = arr[storeIndex], arr[i]
+			storeIndex++
+		}
+	}
+
+	arr[storeIndex], arr[end] = arr[end], arr[storeIndex]
+    return storeIndex
+}
+
 
 func medianOfThree(arr []int, a, b, c int) int {
     if arr[a] < arr[b] {
